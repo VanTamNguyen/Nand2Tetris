da9862ca48eb91414d587e70f0a25a88e9829dc7
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2017-05-08 22:28:33 +0700

    Chapter 4: Get some tatse of machine language

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,47 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Tam-CO's code here:
+
+(LOOP)
+	@SCREEN
+	D=A
+	@i
+	M=D
+
+	@KBD
+	D=M
+	@BLACKEN
+	D;JNE
+	
+	(WHITEN)
+		@i
+		A=M
+		M=0
+		@i
+		M=M+1
+		@i
+		D=M
+		@24575
+		D=D-A
+		@LOOP
+		D;JGT
+		@WHITEN
+		D;JLE
+	
+	
+
+	(BLACKEN)
+		@i
+		A=M
+		M=-1
+		@i
+		M=M+1
+		@i
+		D=M
+		@24575
+		D=D-A
+		@LOOP
+		D;JGT
+		@BLACKEN
+		D;JLE
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Tam-CO's code here:
+
+	@R2
+	M=0
+	
+	@i
+	M=0
+	
+(LOOP)
+	@i
+	D=M
+	@R0
+	D=D-M
+	@END
+	D;JGE
+	
+	@R2
+	D=M
+	@R1
+	D=D+M
+	@R2
+	M=D
+	
+	@i
+	M=M+1
+	
+	@LOOP
+	0;JMP
+	
+(END)
+	@END
+	0;JMP
