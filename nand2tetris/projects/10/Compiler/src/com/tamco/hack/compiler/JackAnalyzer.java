fad67f832142f11185a20ae4085173e3153986cf
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2017-07-12 23:00:51 +0700

    Chapter 10: Compiler part I

--- nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/JackAnalyzer.java
+++ nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/JackAnalyzer.java
@@ -4,4 +4,8 @@ package com.tamco.hack.compiler;
  * Created by tam-co on 12/07/2017.
  */
 public class JackAnalyzer {
+
+	public static void main(String[] args) {
+
+	}
 }
--- nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/JackTokenizer.java
+++ nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/JackTokenizer.java
@@ -2,7 +2,10 @@ package com.tamco.hack.compiler;
 
 import com.tamco.hack.compiler.lexical.Lexical;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +21,28 @@ public class JackTokenizer {
 		this.sourceCode = sourceCode;
 	}
 
-	public List<Lexical> tokenize() {
+	public List<Lexical> tokenize() throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(sourceCode));
+		String line = "";
+		StringBuilder sourceContent = new StringBuilder();
+
+		while ((line = bufferedReader.readLine()) != null) {
+			sourceContent.append(removeLineComment(line)).append("\n");
+		}
+
+		// Remove all comments
+		String code = sourceContent.toString().replaceAll("/\\*.*\\*/", " ");
+
 		List<Lexical> tokens = new ArrayList<>();
 		return tokens;
+	}
+
+	private String removeLineComment(String line) {
+		int index = line.indexOf("//");
+		if (index > -1) {
+			return line.substring(0, index);
+		}
+
+		return line;
 	}
 }
