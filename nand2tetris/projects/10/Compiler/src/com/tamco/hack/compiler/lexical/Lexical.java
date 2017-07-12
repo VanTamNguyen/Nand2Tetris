a0283285e71514b84459df635b513e52e5a3df3d
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2017-07-12 23:15:16 +0700

    Chapter 10: Compiler part I

--- nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/lexical/Lexical.java
+++ nand2tetris/projects/10/Compiler/src/com/tamco/hack/compiler/lexical/Lexical.java
@@ -1,9 +1,23 @@
 package com.tamco.hack.compiler.lexical;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by tam-co on 12/07/2017.
  */
 public class Lexical {
+
+	public static final List<String> keywords = Arrays.asList("class", "constructor", "function",
+															  "method", "field", "static", "var",
+															  "int", "char", "boolean", "void", "true",
+															  "false", "null", "this", "let", "do",
+															  "if", "else", "while", "return");
+
+	public static final List<String> symbols = Arrays.asList("{", "}", "(", ")", "[", "]", ".",
+															 ",", ";", "+", "-", "*", "/", "&",
+															 "|", "<", ">", "=", "~");
+
 
 	public static enum Type {
 		keyword,
@@ -19,6 +33,22 @@ public class Lexical {
 
 	public Lexical(Type type, String lecical) {
 		this.type = type;
+		this.lecical = lecical;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public String getLecical() {
+		return lecical;
+	}
+
+	public void setLecical(String lecical) {
 		this.lecical = lecical;
 	}
 }
